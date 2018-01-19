579e2189160949f5f5e9789b05745fea00d2fb29
Paul T <ptsouchlos@bellsouth.net>
2018-01-18 22:27:49 -0500

    Part 1 - Hello World

---
+++ Part1-HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+	std::cout << "Press any key to exit." << std::endl;
+	int resp;
+	std::cin >> resp;
+	return 0;
+}
