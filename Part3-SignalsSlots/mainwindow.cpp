e57aef1da09e807803d8c8c1b693c5b45ea7c0d6
Paul T <ptsouchlos@bellsouth.net>
2018-09-03 14:57:55 -0400

    Added code for part 4

--- Part3-SignalsSlots/mainwindow.cpp
+++ Part3-SignalsSlots/mainwindow.cpp
@@ -2,7 +2,32 @@
 #include "ui_mainwindow.h"
 
 #include <QFileDialog>
+#include <QImageReader>
+#include <QStandardPaths>
+
 #include <iostream>
+
+inline void initializeImageFileDialog(QFileDialog &dialog)
+{
+	static bool first_dialog = true;
+
+	if (first_dialog) {
+		first_dialog = false;
+		const QStringList pictures_locations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+		dialog.setDirectory(pictures_locations.isEmpty() ? QDir::currentPath() : pictures_locations.last());
+	}
+
+	auto image_types_array = QImageReader::supportedMimeTypes();
+	QStringList mime_type_filters;
+	for (const auto type : image_types_array)
+	{
+		mime_type_filters.append(type);
+	}
+	mime_type_filters.sort();
+
+	dialog.setMimeTypeFilters(mime_type_filters);
+	dialog.selectMimeTypeFilter("image/jpeg");
+}
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,11 +41,20 @@ MainWindow::MainWindow(QWidget *parent) :
 void MainWindow::onOpenActionTriggered()
 {
 	// open a file dialog
-	auto selected_file = QFileDialog::getOpenFileName(this, tr("Open a file"), QApplication::applicationDirPath());
-	
-	if(!selected_file.isNull())
+	QFileDialog file_dialog(this, tr("Open Image"));
+	initializeImageFileDialog(file_dialog);
+	const auto result = file_dialog.exec();
+	if(result == QDialog::Accepted)
 	{
-		std::cout << selected_file.toStdString() << std::endl;
+		// load the file
+		auto path = file_dialog.selectedFiles().first();
+		QImageReader image_reader(path);
+		const QImage image = image_reader.read();
+		if(!image.isNull())
+		{
+			ui->label->setPixmap(QPixmap::fromImage(image));
+			ui->label->resize(ui->label->pixmap()->size());
+		}
 	}
 }
 
